b5f57ce9c2b7a89c7aa3fa7e004e0596f5870be7
Sebastián <sacm1046@gmail.com>
2020-12-13 21:27:31 -0300

    Dark/light improve added

--- js/theme.js
+++ js/theme.js
@@ -10,17 +10,21 @@ const lightTheme = {
     "--light": "white",
     "--dark-soft": "rgba(0,0,0,0.7)",
     "--light-soft": "rgba(255, 255, 255, 0.9)",
-    "--shadow": "rgba(255, 255, 255, .3)"
+    "--btn-primary": "crimson",
+    "--text-primary":"crimson",
+    "--text-light":"white"
 }
 
 const darkTheme = {
     "--primary": "black",
-    "--secondary": "rgb(255, 255, 255)",
+    "--secondary": "rgb(150, 150, 150)",
     "--dark": "rgb(20, 20, 20)",
     "--light": "white",
     "--dark-soft": "rgba(0,0,0,0.7)",
     "--light-soft": "rgba(255, 255, 255, 0.9)",
-    "--shadow": "rgba(255, 255, 255, .3)"
+    "--btn-primary": "white",
+    "--text-primary":"white",
+    "--text-light":"black"
 }
 
 const changeTheme = theme => {
